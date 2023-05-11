d68c4eb20e8f874843b5d5cb59ca4636a67847aa
mazenfteha <mazn34903@gmail.com>
2023-05-10 20:40:45 -0700

    Adding data into redis

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const redis = require('redis');
 
-const redisUrl = "redis://127.0.0.1:6379"
-const client = redis.createClient(redisUrl)
+const util =require('util')
+
+const REDIS_PORT = 6379;
+
+const client = redis.createClient({
+    legacyMode: true,
+    PORT: REDIS_PORT
+});
+
+client
+    .connect()
+    .then(async () => {
+        console.log("You are connected")
+})
+    .catch((err) => {
+        console.log("error happened", err)
+});
+
+client.set = util.promisify(client.set)
 
 const app = express()
 app.use(express.json())
+
+app.post('/', async (req, res) =>{
+    const { key, value } = req.body ;
+    const response = await client.set(key, value)
+    res.json(response)
+})
+
 
 app.listen(3000, ()=>{
     console.log('server running on port 3000');
