7ed93051ec7369cbf859c49fc073551a522a82b6
mazenfteha <mazn34903@gmail.com>
2023-05-10 21:47:13 -0700

    Expring Data

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/posts/:id', async (req,res) => {
     }
 
     const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    client.set(`post-${id}`, JSON.stringify(response.data))
+    client.set(`post-${id}`, JSON.stringify(response.data), "EX", 10)
 
     return res.json(response.data)
 })
